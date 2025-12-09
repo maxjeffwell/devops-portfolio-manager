7222186516194f285f46539a344e5408334b8238
maxjeffwell <maxjeffwell@gmail.com>
2025-12-09 00:01:40 -0500

    Add replace prop to root redirect to fix /devops/ blank page

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             </div>
           }>
             <Routes>
-              <Route path="/" element={<Navigate to="/applications" />} />
+              <Route path="/" element={<Navigate to="/applications" replace />} />
               <Route path="/applications" element={<Applications />} />
               <Route path="/pipelines" element={<Pipelines />} />
               <Route path="/analytics" element={<Analytics />} />
